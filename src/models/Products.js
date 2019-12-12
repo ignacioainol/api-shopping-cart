cd14ff1b8abcf7891df010ae2401f08fd8f65fdf
ignacioainol <iggnaxios@gmail.com>
2019-12-12 18:30:26 -0300

    products almost ok endoints

--- src/models/Products.js
+++ src/models/Products.js
@@ -23,7 +23,52 @@ const getProductById = async (id) => {
     return result.rows;
 }
 
+const save = async (name, price, description) => {
+    const conexion = await conn();
+    const query = `
+        INSERT INTO products (name, price, description)
+        VALUES ($1, $2, $3)
+        RETURNING *
+    `;
+
+    const result = await conexion.query(query, [name, price, description])
+    conexion.release();
+    return result.rows;
+}
+
+const update = async (id, productData) => {
+    const conexion = await conn();
+    const query = `
+        UPDATE products
+        SET name = $1, price = $2, description = $3
+        WHERE id = $4
+        RETURNING *
+    `;
+
+    const result = await conexion.query(query, [productData.name, productData.price, productData.description, id]);
+    conexion.release();
+
+    return result.rows[0];
+}
+
+const remove = async (id) => {
+    const conexion = await conn();
+    const query = `
+        DELETE
+        FROM products 
+        WHERE id = $1
+        RETURNING *`;
+
+    const result = await conexion.query(query, [id]);
+    conexion.release();
+    return result;
+
+}
+
 module.exports = {
     getAll,
-    getProductById
+    getProductById,
+    save,
+    remove,
+    update
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -21,9 +21,47 @@ router.get('/:id', async (req,res) => {
     } 
 });
 
-router.post('/',  (req,res) => {
-    const nombre = req.body.name;
-    res.send(nombre);
+router.post('/', async  (req,res) => {
+    try {
+        const { name, price, description } = req.body;
+
+        const newProduct = await productsModel.save(name, price, description);
+        res.status(201).send(newProduct);
+
+    } catch (error) {
+        res.status(500).send(error.message);
+    }    
 });
+
+router.put('/:id', async (req,res) => {
+    try {
+        const productId = req.params.id;
+        const product = productsModel.getProductById(productId);
+
+        if(product){
+            const { body: productData } = req;
+            const productEdited = await productsModel.update(productId, productData);
+            res.status(200).send(productEdited);
+        }
+
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+router.delete('/:id', async (req,res) => {
+    try {
+        const productId = req.params.id;
+        const product = await productsModel.getProductById(productId);
+        if(product){
+            await productsModel.remove(productId);
+            res.status(200).send(product);
+        }else{
+            res.status(404).send();
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+})
 
 module.exports = router;
